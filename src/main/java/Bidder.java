202402df1fbe2e1d6fe3a2e760ab10ef7a3046b9
hunterwb <hunterwb@washington.edu>
2016-11-08 10:07:56 -0800

    initial ui

---
+++ src/main/java/Bidder.java
@@ -0,0 +1,11 @@
+public class Bidder extends User {
+
+    public Bidder(String username, String name) {
+        super(username, name);
+    }
+
+    @Override
+    public String getTitle() {
+        return "Bidder";
+    }
+}
