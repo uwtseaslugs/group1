b699cf96ddb47fa5ccc021b8be282680d98438a4
hunterwb <hunterwb@washington.edu>
2016-11-10 23:18:27 -0800

    added junit, initial Item tests

--- src/test/java/auctioncentral/model/ItemTest.java
+++ src/test/java/auctioncentral/model/ItemTest.java
@@ -1,4 +1,23 @@
 package auctioncentral.model;
 
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class ItemTest {
+
+    @Test
+    public void testNewItemPositiveMinimumBid() {
+        Item item = new Item("name", ItemCondition.GOOD, ItemSize.SMALL, 50, null, null, null);
+        assertEquals(item.getMinimumBid(), 50);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNewItemNegativeMinimumBid() {
+        Item item = new Item("name", ItemCondition.GOOD, ItemSize.SMALL, -50, null, null, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNewItemZeroMinimumBid() {
+        Item item = new Item("name", ItemCondition.GOOD, ItemSize.SMALL, 0, null, null, null);
+    }
 }
