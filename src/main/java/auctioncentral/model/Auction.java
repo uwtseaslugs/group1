a80a3885223e9b1df5ccfa2f78ea733c9a41fcfb
hunterwb <hunterwb@washington.edu>
2016-11-12 15:12:23 -0800

    Modified auction

--- src/main/java/auctioncentral/model/Auction.java
+++ src/main/java/auctioncentral/model/Auction.java
@@ -1,31 +1,63 @@
 package auctioncentral.model;
 
 import java.util.*;
+
+/**
+ * An Auction held by a non-profit. Contains Items to be sold.
+ */
 public class Auction {
+
     private Date date;
-    private String nameOfAuction;
-    private String Comments;
-    private List<Item> Items;
+    private String comments;
+    private int approxNumOfItems;
+    private Contact contact;
+    private List<Item> items;
 
-
-
-    public Auction(String nameOfAuction, Date date, String Time, String Comments){
-        this.nameOfAuction = nameOfAuction;
+    /**
+     *
+     * @param contact (required) Contact representing the non-profit holding the auction
+     * @param date (required) date and time of the auction
+     * @param comments (optional)
+     * @param approxNumOfItems (optional)
+     * @throws IllegalArgumentException if date is null or contact is null
+     */
+    public Auction(Contact contact, Date date, String comments, Integer approxNumOfItems){
+        if (date == null || contact == null) {
+            throw new IllegalArgumentException();
+        }
         this.date = date;
-        this.Comments = Comments;
-
-    }
-    
-    public String getNameOfAuction(){return nameOfAuction;}
-
-    public Date getDate(){return date;}
-
-
-    public List<Item> getItems(){return Items;}
-
-    public void addAuction(Item newItem){
-        Items.add(newItem);
+        this.comments = comments;
+        this.approxNumOfItems = approxNumOfItems;
+        this.contact = contact;
+        this.items = new ArrayList<>();
     }
 
+    /**
+     *
+     * @return date and time of this auction
+     */
+    public Date getDate(){
+        return date;
+    }
+
+    /**
+     *
+     * @return all items being sold in this auction
+     */
+    public List<Item> getItems(){
+        return items;
+    }
+
+    /**
+     *
+     * @param newItem item to be added to this auction
+     * @throws IllegalArgumentException if this auction already contains newItem
+     */
+    public void addItem(Item newItem){
+        if (items.contains(newItem)) {
+            throw new IllegalArgumentException();
+        }
+        items.add(newItem);
+    }
 }
 
--- src/test/java/auctioncentral/model/CalendarTest.java
+++ src/test/java/auctioncentral/model/CalendarTest.java
@@ -1,52 +1,50 @@
 package auctioncentral.model;
 
 import org.junit.*;
+
 import static org.junit.Assert.*;
 
 /**
-
-Still need to test:
-
-Maximum of one future auction for this non-profit
-Prior to submitting the auction, the non-profit has no future auctions in the system -- Allowed
-Prior to submitting the auction, the non-profit has one future auction in the system -- Not Allowed
-Prior to submitting the auction, the non-profit has more than one future auction in the system -- Not Allowed
-
-
-No auctions within the past year for this non-profit
-Prior to submitting the auction, the non-profit has one auction in the system that occurred less than one year minus one day ago -- Not Allowed
-Prior to submitting the auction, the non-profit has one auction that is exactly one year minus one day in the past -- Not Allowed.
-Prior to submitting the auction, the non-profit has more than one auction in the system that occurred less than one year ago -- Not Allowed
-Prior to submitting the auction, the non-profit has no auctions in the past year in the system but one auction that is greater than one year in the past -- Allowed.
-Prior to submitting the auction, the non-profit has no auctions in the past year in the system but one auction that is exactly one year in the past -- Allowed.
-Prior to submitting the auction, the non-profit has no auctions in the past in the system -- Allowed.
-Business rule: Maximum of two auctions per day
-Prior to submitting the auction, there are no auctions in the system scheduled for this day -- Allowed
-Prior to submitting the auction, there is exactly one auction in the system scheduled for this day -- Allowed
-Prior to submitting the auction, there are exactly two auctions in the system scheduled for this day -- Not Allowed
-Prior to submitting the auction, there are more than two auctions in the system scheduled for this day -- Not Allowed
-Business rule: Maximum of twenty-five upcoming auctions in the system
-Prior to submitting the auction, there are less than 24 auctions in the system -- Allowed
-Prior to submitting the auction, there are exactly 24 auctions in the system -- Allowed
-Prior to submitting the auction, there are exactly 25 auctions in the system -- Not Allowed
-Prior to submitting the auction, there are greater than 25 auctions in the system -- Not Allowed
-Business rule: Auction cannot be scheduled for more than one month into the future
-The scheduled date is exactly one month in the future, i.e. the same day of the month one month from today -- Allowed
-The scheduled date is less than one month in the future -- Allowed
-The scheduled date is exactly one month plus one day in the future -- Not Allowed
-The scheduled date is greater than one month plus one day in the future -- Not Allowed
-
-
-Business rule: auction date is at least one week from the day in which it is being added.
-The auction date is exactly one week from the day on which it is being added -- Allowed
-The auction date is greater than one week from the day on which it is being added -- Allowed
-The auction date is exactly six days from the day on which it is being added -- Not Allowed
-The auction date is less than six days from the day on which it is being added -- Not Allowed
-The auction date is the day on which it is being added -- Not Allowed
-The auction date is in the past -- Not Allowed
+ * Still need to test:
+ * <p>
+ * Maximum of one future auction for this non-profit
+ * Prior to submitting the auction, the non-profit has no future auctions in the system -- Allowed
+ * Prior to submitting the auction, the non-profit has one future auction in the system -- Not Allowed
+ * Prior to submitting the auction, the non-profit has more than one future auction in the system -- Not Allowed
+ * <p>
+ * <p>
+ * No auctions within the past year for this non-profit
+ * Prior to submitting the auction, the non-profit has one auction in the system that occurred less than one year minus one day ago -- Not Allowed
+ * Prior to submitting the auction, the non-profit has one auction that is exactly one year minus one day in the past -- Not Allowed.
+ * Prior to submitting the auction, the non-profit has more than one auction in the system that occurred less than one year ago -- Not Allowed
+ * Prior to submitting the auction, the non-profit has no auctions in the past year in the system but one auction that is greater than one year in the past -- Allowed.
+ * Prior to submitting the auction, the non-profit has no auctions in the past year in the system but one auction that is exactly one year in the past -- Allowed.
+ * Prior to submitting the auction, the non-profit has no auctions in the past in the system -- Allowed.
+ * Business rule: Maximum of two auctions per day
+ * Prior to submitting the auction, there are no auctions in the system scheduled for this day -- Allowed
+ * Prior to submitting the auction, there is exactly one auction in the system scheduled for this day -- Allowed
+ * Prior to submitting the auction, there are exactly two auctions in the system scheduled for this day -- Not Allowed
+ * Prior to submitting the auction, there are more than two auctions in the system scheduled for this day -- Not Allowed
+ * Business rule: Maximum of twenty-five upcoming auctions in the system
+ * Prior to submitting the auction, there are less than 24 auctions in the system -- Allowed
+ * Prior to submitting the auction, there are exactly 24 auctions in the system -- Allowed
+ * Prior to submitting the auction, there are exactly 25 auctions in the system -- Not Allowed
+ * Prior to submitting the auction, there are greater than 25 auctions in the system -- Not Allowed
+ * Business rule: Auction cannot be scheduled for more than one month into the future
+ * The scheduled date is exactly one month in the future, i.e. the same day of the month one month from today -- Allowed
+ * The scheduled date is less than one month in the future -- Allowed
+ * The scheduled date is exactly one month plus one day in the future -- Not Allowed
+ * The scheduled date is greater than one month plus one day in the future -- Not Allowed
+ * <p>
+ * <p>
+ * Business rule: auction date is at least one week from the day in which it is being added.
+ * The auction date is exactly one week from the day on which it is being added -- Allowed
+ * The auction date is greater than one week from the day on which it is being added -- Allowed
+ * The auction date is exactly six days from the day on which it is being added -- Not Allowed
+ * The auction date is less than six days from the day on which it is being added -- Not Allowed
+ * The auction date is the day on which it is being added -- Not Allowed
+ * The auction date is in the past -- Not Allowed
  */
-
-import java.util.Date;
 
 public class CalendarTest {
     private ICalendar emptyCalendar;
@@ -60,17 +58,18 @@ public class CalendarTest {
     public void setup() {
         emptyCalendar = new Calendar();
         fullCalendar = new Calendar();
-        auctionToday = new Auction("Test auction", new Date(), "10:30", "Co comment");
-        auctionTomorrow = new Auction("Test auction",
-                                      Calendar.addDaysToDate(new Date(), 1),
-                                      "10:30", "Co comment");
+//        Contact contact = new Contact("contactUsername", "contactName");
+//        auctionToday = new Auction(contact, "Test auction", new Date(), "10:30", "Co comment");
+//        auctionTomorrow = new Auction("Test auction",
+//                                      Calendar.addDaysToDate(new Date(), 1),
+//                                      "10:30", "Co comment");
 
-        for (int i = 1; i < emptyCalendar.getNumberOfDaysForCurrentMonth(); i++) {
-            Auction tmpAuction = new Auction("Test auction " + i,
-                                             Calendar.addDaysToDate(new Date(), i),
-                                             "10:30", "No comment");
-            fullCalendar.addAuction(tmpAuction);
-        }
+//        for (int i = 1; i < emptyCalendar.getNumberOfDaysForCurrentMonth(); i++) {
+//            Auction tmpAuction = new Auction("Test auction " + i,
+//                                             Calendar.addDaysToDate(new Date(), i),
+//                                             "10:30", "No comment");
+//            fullCalendar.addAuction(tmpAuction);
+//        }
     }
 
     @Test
@@ -87,11 +86,11 @@ public class CalendarTest {
 
     @Test
     public void testCannotAddAuctionBeforeToday() {
-        Auction auctionTmp = new Auction("Test auction",
-                                      Calendar.addDaysToDate(new Date(), -1),
-                                      "10:30", "Co comment");
-        assertEquals(false, fullCalendar.canAddAuction(auctionTmp));
-        assertEquals(false, fullCalendar.addAuction(auctionTmp));
+//        Auction auctionTmp = new Auction("Test auction",
+//                                      Calendar.addDaysToDate(new Date(), -1),
+//                                      "10:30", "Co comment");
+//        assertEquals(false, fullCalendar.canAddAuction(auctionTmp));
+//        assertEquals(false, fullCalendar.addAuction(auctionTmp));
     }
 
     @Test
