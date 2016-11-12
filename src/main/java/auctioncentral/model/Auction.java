c9e9ccb4834b5f010a146a08e6e34c1923ab9fc5
Jonchac <jonchac@hotmail.com>
2016-11-11 17:21:38 -0800

    Update auction.java
    Added some stuff still gonna work on it.

--- src/main/java/auctioncentral/model/Auction.java
+++ src/main/java/auctioncentral/model/Auction.java
@@ -1,5 +1,30 @@
 package auctioncentral.model;
 
+import java.util.*;
 public class Auction {
-    
+    private Date date;
+    private String nameOfAuction;
+    private String Comments;
+    private List<Item> Items;
+
+
+
+    public Auction(String nameOfAuction,Date date, String Time, String Comments){
+        this.nameOfAuction = nameOfAuction;
+        this.date = date;
+        this.Comments = Comments;
+
+    }
+    public String getNameOfAuction(){return nameOfAuction;}
+
+    public Date getDate(){return date;}
+
+
+    public List<Item> getItems(){return Items;}
+
+    public void addAuction(Item newItem){
+        Items.add(newItem);
+    }
+
 }
+
