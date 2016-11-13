42403790563976bf35479695d42b43b2f5d90dd1
hunterwb <hunterwb@washington.edu>
2016-11-13 14:44:50 -0800

    added serialization methods, moved auction comparator

--- src/main/java/auctioncentral/AuctionCentral.java
+++ src/main/java/auctioncentral/AuctionCentral.java
@@ -3,20 +3,50 @@ package auctioncentral;
 import auctioncentral.model.*;
 import auctioncentral.view.login.*;
 
+import java.io.*;
+
 public class AuctionCentral {
 
     public static ILoginManager loginManager = new LoginManager();
     public static ICalendar calendar = new Calendar();
 
     public static void main(String[] args) {
+//        calendar = (ICalendar) deserializeFrom("cal.ser");
         addAuctions();
         new LoginUsernameMenu().show();
+        serializeTo(calendar, "cal.ser");
+    }
+
+    private static void serializeTo(Object o, String fileName) {
+        try {
+            FileOutputStream fos = new FileOutputStream(fileName);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(o);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Object deserializeFrom(String fileName) {
+        try {
+            FileInputStream fis = new FileInputStream(fileName);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Object o = ois.readObject();
+            ois.close();
+            fis.close();
+            return o;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     // test
     private static void addAuctions() {
         java.util.Calendar c = java.util.Calendar.getInstance();
-        c.add(java.util.Calendar.DATE, 8);
+        c.add(java.util.Calendar.DATE, 13);
         for (int i = 0; i < 12; i++) {
             c.add(java.util.Calendar.DATE, 1);
             calendar.addAuction(new Auction(new Contact("" + i, "" + i), c.getTime(), null, null));
--- src/main/java/auctioncentral/model/Calendar.java
+++ src/main/java/auctioncentral/model/Calendar.java
@@ -8,17 +8,7 @@ public class Calendar implements ICalendar {
     private TreeSet<Auction> auctions;
 
     public Calendar() {
-        auctions = new TreeSet<>((new Comparator<Auction>() {
-            @Override
-            public int compare(Auction o1, Auction o2) {
-                int compareDates = o1.getDate().compareTo(o2.getDate());
-                if (compareDates == 0) {
-                    return Integer.compare(o1.hashCode(), o2.hashCode());
-                } else {
-                    return compareDates;
-                }
-            }
-        }));
+        auctions = new TreeSet<>();
     }
 
     @Override
