3b667db109ea5150a21ead380f097cd0d47c77ef
Jonchac <jonchac@hotmail.com>
2016-11-15 00:18:10 -0800

    Update Staff.java

--- src/main/java/auctioncentral/model/Staff.java
+++ src/main/java/auctioncentral/model/Staff.java
@@ -1,3 +1,10 @@
+/*
+    Deliverable 2 
+    TCSS 360
+    Created by: Sea Slugs
+    
+    Staff class takes in the username and name.
+ */
 package auctioncentral.model;
 
 public class Staff extends User {
