718946764488eaf9377d6e6167f5470a2863acdb
Jonchac <jonchac@hotmail.com>
2016-11-14 19:09:59 -0800

    Update ICalendar.java

--- src/main/java/auctioncentral/model/ICalendar.java
+++ src/main/java/auctioncentral/model/ICalendar.java
@@ -9,7 +9,9 @@ public interface ICalendar extends Serializable {
     boolean canAddAuction(Auction a);
     boolean addAuction(Auction a);
     boolean removeAuction(Auction a);
-
+    boolean faddAuction(Auction a);
+    boolean canAddAuctionYear(Auction a);
+    
     List<Auction> getAuctionsPastDate(Date d);
     List<Auction> getAuctionsBetweenDates(Date start, Date end);
 
