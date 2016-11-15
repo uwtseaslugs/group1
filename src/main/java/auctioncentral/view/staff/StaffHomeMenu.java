236440a769bb070eb881ad34ef50481fb96510ab
Jonchac <jonchac@hotmail.com>
2016-11-15 01:31:50 -0800

    Update StaffHomeMenu.java

--- src/main/java/auctioncentral/view/staff/StaffHomeMenu.java
+++ src/main/java/auctioncentral/view/staff/StaffHomeMenu.java
@@ -1,3 +1,10 @@
+/*
+    Deliverable 2 
+    TCSS 360
+    Created by: Sea Slugs
+    
+    StaffHomeMenu is the home screen for the staff.
+ */
 package auctioncentral.view.staff;
 
 import auctioncentral.*;
