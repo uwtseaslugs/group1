7ea4c1505047157565d4a3fb7cd9c5bc57c90786
Jonchac <jonchac@hotmail.com>
2016-11-15 01:24:59 -0800

    Update ContactNonprofitNameMenu.java

--- src/main/java/auctioncentral/view/contact/ContactNonprofitNameMenu.java
+++ src/main/java/auctioncentral/view/contact/ContactNonprofitNameMenu.java
@@ -1,3 +1,10 @@
+/*
+    Deliverable 2 
+    TCSS 360
+    Created by: Sea Slugs
+    
+    ContactNonprofitNameMenu gets the name of the non-profit.
+ */
 package auctioncentral.view.contact;
 
 import auctioncentral.*;
