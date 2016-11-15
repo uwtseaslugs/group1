55343f74b210900fb00d3dd97a5f3977f9d14bf2
Jonchac <jonchac@hotmail.com>
2016-11-14 19:56:07 -0800

    Update Contact.java
    added java doc

--- src/main/java/auctioncentral/model/Contact.java
+++ src/main/java/auctioncentral/model/Contact.java
@@ -3,20 +3,37 @@ package auctioncentral.model;
 public class Contact extends User {
 
     private String nonprofitName;
-
+    
+    /**
+     *
+     * @param username
+     * @param name
+     * Sets the username and name of Contact
+     */
     public Contact(String username, String name) {
         this(username, name, "");
     }
-
+        /**
+     *
+     * @param username
+     * @param name
+     */
     public Contact(String username, String name, String nonprofitName) {
         super(username, name);
         this.nonprofitName = nonprofitName;
     }
-
+    
+    /**
+     *
+     * @return nonprofit name
+     */
     public String getNonprofitName() {
         return nonprofitName;
     }
-
+        /**
+     *
+     * @return gets the title
+     */
     @Override
     public String getTitle() {
         return "Contact";
