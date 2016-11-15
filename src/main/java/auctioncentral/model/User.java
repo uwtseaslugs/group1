d45a68af80bd76f527f667923213877fe5681a98
Jonchac <jonchac@hotmail.com>
2016-11-14 23:59:01 -0800

    Update User.java

--- src/main/java/auctioncentral/model/User.java
+++ src/main/java/auctioncentral/model/User.java
@@ -6,16 +6,26 @@ public abstract class User implements Serializable {
 
     protected String username;
     protected String name;
-
+    /**
+     *
+     * @param username
+     * @param name
+     */
     protected User(String username, String name) {
         this.username = username;
         this.name = name;
     }
-
+    /**
+     *
+     * @return gets the username
+     */
     public String getUsername() {
         return username;
     }
-
+    /**
+     * 
+     * @return gets the name of user
+     */
     public String getName() {
         return name;
     }
