3539b34d21649002268d0c560f56cca72ff74bbc
hunterwb <hunterwb@washington.edu>
2016-12-05 10:52:20 -0800

    AddAuction input validation

--- src/main/java/auctioncentral/gui/contact/ContactAddAuctionView.java
+++ src/main/java/auctioncentral/gui/contact/ContactAddAuctionView.java
@@ -13,6 +13,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Observable;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Created by Jon
@@ -27,6 +29,8 @@ public class ContactAddAuctionView extends AbstractScreen {
     private JLabel numOfItemsLabel;
 
     private JTextField dateText;
+    private JComboBox<String> hourCombo;
+    private JComboBox<String> amPmCombo;
     private JTextField commentsText;
     private JTextField numOfItemsText;
 
@@ -34,45 +38,57 @@ public class ContactAddAuctionView extends AbstractScreen {
     private JButton returnHomeButton;
 
     private Date dateParsed;
-    private String dateAndTime;
+    private String date;
     private String comments;
     private String tempItems;
-    private int amountOfItems;
+    private Integer amountOfItems;
 
 
     public ContactAddAuctionView(){
-        auctionTitle = new JLabel("Date and Time of Auction: (MM/DD/YYYY HH [AM/PM])");
-        CommentLabel = new JLabel("Enter in comments for auction: ");
-        numOfItemsLabel = new JLabel("Enter in the approximate number of Items in the autcion: ");
+        auctionTitle = new JLabel("Date and Time of Auction: (MM/DD/YYYY)");
+        hourCombo = new JComboBox<>(IntStream.range(1, 13).mapToObj(a -> "" + a).collect(Collectors.toList()).toArray(new String[0]));
+        amPmCombo = new JComboBox<>(new String[]{"AM", "PM"});
+        CommentLabel = new JLabel("Comments (Optional):  ");
+        numOfItemsLabel = new JLabel("Approximate number of Items (Optional): ");
 
-        dateText = new JTextField(40);
-        commentsText = new JTextField(40);
-        numOfItemsText = new JTextField(40);
+        dateText = new JTextField(10);
+        commentsText = new JTextField(10);
+        numOfItemsText = new JTextField(3);
 
         ComfirmButton = new JButton("Request Auction");
         returnHomeButton = new JButton("Return Home");
 
         ComfirmButton.addActionListener(e -> {
             SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy hh a");
-            dateAndTime = dateText.getText();
+            date = dateText.getText();
             comments = commentsText.getText();
             tempItems = numOfItemsText.getText();
-            amountOfItems = Integer.parseInt(tempItems);
+            if (tempItems.trim().equals("")) {
+                amountOfItems = null;
+            } else {
+                try {
+                    amountOfItems = Integer.parseInt(tempItems);
+                } catch (NumberFormatException exc) {
+                    JOptionPane.showMessageDialog(this, "Number of items must be a positive number");
+                    return;
+                }
+            }
             try{
-                dateParsed = format.parse(dateAndTime);
+                dateParsed = format.parse(date + " " + hourCombo.getSelectedItem() + " " + amPmCombo.getSelectedItem());
             }catch (ParseException a) {
                 JOptionPane.showMessageDialog(this,"Please enter date in valid format.");
-
+                return;
             }
             try {
                 newAuction = new Auction((Contact) LoginManager.inst().getCurrentUser(), dateParsed, comments, amountOfItems);
             } catch (IllegalArgumentException b) {
-                JOptionPane.showMessageDialog(this,"Error inputting information. Press Ok to return to previous menu.");
+                JOptionPane.showMessageDialog(this,"Error inputting information.");
+                return;
             }
             if(!Calendar.inst().canAddAuctionYear(newAuction)){
                 JOptionPane.showMessageDialog(this,"Your Auction request is denied because you have had an Auction within the past year.");
             }else if(!Calendar.inst().canAddAuctionDailyLimit(newAuction)){
-                JOptionPane.showMessageDialog(this,"Your Auction request is denied because you their is already 2 auction scheduled for this date.");
+                JOptionPane.showMessageDialog(this,"Your Auction request is denied because there are already 2 auction scheduled for this date.");
             }else if(!Calendar.inst().canAddAuctionMaxInFuture(newAuction)){
                 JOptionPane.showMessageDialog(this,"Your Auction request is denied because we do not schedule further than one month ahead.");
             }else if(!Calendar.inst().canAddAuctionMaxLimit(newAuction)){
@@ -80,13 +96,12 @@ public class ContactAddAuctionView extends AbstractScreen {
             }else if(!Calendar.inst().canAddAuctionNonprofitLimit(newAuction)){
                 JOptionPane.showMessageDialog(this,"Your Auction request is denied because you already have an auction in the system.");
             }else if(!Calendar.inst().canAddAuctionMinInFuture(newAuction)){
-                JOptionPane.showMessageDialog(this,"Your Auction request is denied because you need to schedule your auction 1 week ahead.");
+                JOptionPane.showMessageDialog(this,"Your Auction request is denied because you need to schedule your auction at least 1 week ahead.");
             }else{
-                JOptionPane.showMessageDialog(this,"Sucessfully added your Auction on :" + dateAndTime);
+                JOptionPane.showMessageDialog(this,"Successfully added your Auction on: " + date);
                 Calendar.inst().addAuction(newAuction);
                 getRoot().addScreen(new ContactHomeView());
             }
-
         });
         returnHomeButton.addActionListener(e -> {
             getRoot().addScreen(new ContactHomeView());
@@ -95,15 +110,18 @@ public class ContactAddAuctionView extends AbstractScreen {
         Box vertBox = Box.createVerticalBox();
         Box row1 = Box.createHorizontalBox();
         row1.add(auctionTitle);
-        row1.add(Box.createRigidArea(new Dimension(0,5)));
+        row1.add(Box.createRigidArea(new Dimension(30,0)));
         row1.add(Box.createGlue());
         row1.add(dateText);
+        row1.add(hourCombo);
+        row1.add(amPmCombo);
         Box row2 = Box.createHorizontalBox();
         row2.add(CommentLabel);
         row2.add(Box.createGlue());
         row2.add(commentsText);
         Box row3 = Box.createHorizontalBox();
         row3.add(numOfItemsLabel);
+        row3.add(Box.createRigidArea(new Dimension(205, 0)));
         row3.add(Box.createGlue());
         row3.add(numOfItemsText);
         vertBox.add(row1);
