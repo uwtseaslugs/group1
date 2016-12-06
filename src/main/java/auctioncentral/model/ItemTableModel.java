a50b7040e994102d2e0b86ed561628803935bdec
hunterwb <hunterwb@washington.edu>
2016-12-06 07:30:03 -0800

    dollar signs added

--- src/main/java/auctioncentral/model/ItemTableModel.java
+++ src/main/java/auctioncentral/model/ItemTableModel.java
@@ -26,7 +26,7 @@ public class ItemTableModel extends AbstractTableModel {
         for (int i = 0; i < itemList.size(); i++){
             item = itemList.get(i);
             data[i][0] = item.getName();
-            data[i][1] = item.getMinimumBid();
+            data[i][1] = "$" + item.getMinimumBid();
             data[i][2] = item.getCondition();
             data[i][3] = item.getBid((Bidder) LoginManager.inst().getCurrentUser());
         }
